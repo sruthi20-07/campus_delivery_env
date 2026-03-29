2b63f8a0bb1ba406e3bbcef6f41119806e7837a1
sruthi20-07 <vindulasruthi20@gmail.com>
2026-03-29 23:08:53 +0530

    fix main callable

--- server/app.py
+++ server/app.py
@@ -7,10 +7,8 @@ app = FastAPI()
 def home():
     return {"status": "running"}
 
-
 def main():
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
-
 
 if __name__ == "__main__":
     main()
